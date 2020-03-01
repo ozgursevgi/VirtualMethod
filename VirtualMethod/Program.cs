d3c91f75c63d2d6d2878c68fac9bd811c64645b6
Özgür Fidan <ozgursevgi@msn.com>
2020-03-01 15:18:49 +0300

    Add project files.

---
+++ VirtualMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sql =new SqlServer();
+            sql.Add();
+            MysqlServer mys=new MysqlServer();
+            mys.Add();
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Aded by deafult");    
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by deafult");
+        }
+    }
+
+    class SqlServer:Database
+    {
+
+    }
+    class MysqlServer : Database
+    {
+
+    }
+
+}
